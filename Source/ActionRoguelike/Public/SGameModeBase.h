cd5b28a35e4018de8c0bdcb1681fa68d74a4d9ea
Marcelo Mendez <83145704+Marcelo-Mendez624@users.noreply.github.com>
2023-10-02 18:43:47 -0300

    Setting up game mode to spawn enemies

---
+++ Source/ActionRoguelike/Public/SGameModeBase.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "GameFramework/GameModeBase.h"
+#include "SGameModeBase.generated.h"
+
+/**
+ * 
+ */
+
+class UEnvQueryInstanceBlueprintWrapper;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+
+	ASGameModeBase();
+	
+	virtual void StartPlay() override;
+
+	
+protected:
+	FTimerHandle TimerHandle_SpawnBots;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	float SpawnTimerInterval;
+
+	
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	TSubclassOf<AActor> MinionClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	class UCurveFloat* DifficultyCurve;
+	
+	UFUNCTION()
+	void SpawnBotTimerElapsed();
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	class UEnvQuery* SpawnBotQuery;
+
+	UFUNCTION()
+	void OnQueryCompleted(UEnvQueryInstanceBlueprintWrapper* QueryInstance, EEnvQueryStatus::Type QueryStatus);
+
+	
+
+};
